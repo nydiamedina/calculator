7f33bb6450f06cbfdc3e82430ab69fe2c144d549
Nydia Medina <nvmedina29@gmail.com>
2021-11-13 15:47:24 -0600

    Square root implementation working

--- calc.js
+++ calc.js
@@ -34,6 +34,10 @@ reader.question('What would you like to calculate?', function(input){
 		console.log(num1 / num2);
 	}
 
+	if (mathSymbol === '^') {
+		console.log(Math.sqrt(num1));
+	}
+
 	// This line closes the connection to the command line interface.
 	reader.close()
 
